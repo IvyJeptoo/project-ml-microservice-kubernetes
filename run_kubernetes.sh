31f80042545fd87bdd0fbaf6aacb9def2ea5a78a
EC2 Default User <ec2-user@ip-172-31-77-254.ec2.internal>
2023-09-18 21:31:18 +0000

    deployment file to kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,10 +4,10 @@
 
 # Step 1:
 # Define your Docker repository and tag
-dockerpath="jeptoo/ivyudacitymicroservice:latest"
+dockerpath=jeptoo/ivyudacitymicroservice:latest
 
 # Step 2:
-# Create a Kubernetes deployment
+# Run the Docker Hub container with kubernetes
 kubectl create deployment ivyudacitymicroservice --image=$dockerpath
 
 # Step 3:
@@ -16,12 +16,4 @@ kubectl get pods
 
 # Step 4:
 # Create a Kubernetes service (LoadBalancer) to expose the deployment
-kubectl expose deployment ivyudacitymicroservice --type=LoadBalancer --port=80 --target-port=80
-
-# Step 5:
-# Wait for the service's external IP to be assigned (might take a moment)
-kubectl wait --for=condition=available --timeout=300s service/ivyudacitymicroservice
-
-# Step 6:
-# Open the service in a web browser (Minikube specific)
-minikube service ivyudacitymicroservice
+kubectl port-forward deployment/ivyudacitymicroservice 8000:80
